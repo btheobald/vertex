ee70a6972bc438b4b63de15031cecbf49b4c4a91
Byron Theobald <byrontheobald@gmail.com>
2017-03-16 22:20:47 +0000

    Started writing basic todos.

--- vtx_core.py
+++ vtx_core.py
@@ -7,16 +7,22 @@ should be split out into appropriate modules and imported here.
 Keep cyclic imports to minimum, ie write in a way where modules are not interlinked.
 """
 
-# IMPORTS
+# EXTERNAL LIBRARIES
+from Tkinter import *
+
+# INTERNAL MODULES
 from module import vtx_calc
 from module import vtx_draw
 from module import vtx_file
 from module import vtx_gui
 from module import vtx_com
 
-# TEST MODULES
+# INTEGRATION TEST MODULES
 # TODO: Remove from final version.
 from tests import tests_core
 
 tests_core.module_test()
 
+# TODO: Initialise Tkinter Window
+# Initially write basic setup in this file but migrate to module/vtx_gui.py.
+# TODO: Write GUI init function in vtx_gui, call here. Return window handler.
