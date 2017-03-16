cd47de6f167587dab9bf2a0f508dca9b9f9ac978
Byron Theobald <byrontheobald@gmail.com>
2017-03-16 20:55:31 +0000

    Added vecXY class into vtx_pcom.py

--- vtx_pcom.py
+++ vtx_pcom.py
@@ -4,3 +4,27 @@ Program common module.
 This module can be imported to all modules to provide common
 access to container / object definitions for modules.
 """
+
+class vecXY:
+    """A simple two dimensional vector container"""
+    __vecX = 0.00
+    __vecY = 0.00
+
+    def set(self, nX, nY):
+        """Set the velocity values"""
+        self.__vecX = nX
+        self.__vecY = nY
+
+    def sum(self, sX, sY):
+        """Sum onto the current velocity values"""
+        self.__vecX += sX
+        self.__vecY += sY
+
+    def get(self, xy = None):
+        """Returns the current velocity, either in a set or individually"""
+        if (xy == None):
+          return [self.__vecX, self.__vecY]
+        elif xy == 0:
+          return self.__vecX
+        else:
+          return self.__vecY
