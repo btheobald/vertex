db695be092654dfe2c3e5028bd7c4e6540f9659c
Byron Theobald <byrontheobald@gmail.com>
2017-03-16 16:42:51 +0000

    Added a test module with module implementation test

--- vtx_core.py
+++ vtx_core.py
@@ -4,3 +4,9 @@ Program core / startup module.
 Parts of the program not relating directly to startup of the program
 should be split out into appropriate modules and imported here.
 """
+
+# IMPORTS
+import vtx_test
+
+vtx_test.module_test()
+
