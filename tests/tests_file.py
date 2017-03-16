39ff692a156684e29e2024fa6ac72670d5eaee65
Byron Theobald <byrontheobald@gmail.com>
2017-03-16 23:20:39 +0000

    Made test 1 printout more fancy.

--- tests/tests_file.py
+++ tests/tests_file.py
@@ -1,7 +1,7 @@
 from module import vtx_file
 from module import vtx_com
 
-# Test load JSON point data.
+# Test load JSON point data and check resulting objects.
 saveData = vtx_file.loadJSONData('test1.json')
 pointSet = vtx_file.initPoints(saveData)
 print pointSet
@@ -10,14 +10,14 @@ if len(pointSet) == 2:
 else:
     print "Test load JSON point data: FAIL"
 
-print pointSet[0].pPos.get()
+print "Object Data:", str(pointSet[0].pPos.get())
 if pointSet[0].pPos.get() == [100,200]:
-    print "Point[0] position data match: PASS"
+    print "Point[0] position [100,200] data match: PASS"
 else:
-    print "Point[0] position data match: FAIL [100,200]"
+    print "Point[0] position [100,200] data match: FAIL "
 
-print pointSet[1].pPos.get()
+print "Object Data:", str(pointSet[1].pPos.get())
 if pointSet[1].pPos.get() == [300,200]:
-    print "Point[1] position data match: PASS"
+    print "Point[1] position [300,200] data match: PASS"
 else:
-    print "Point[1] position data match: FAIL [300,200]"
+    print "Point[1] position [300,200] data match: FAIL "
