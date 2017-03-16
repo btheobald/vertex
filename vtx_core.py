897be071927c8ce962167db63be870cae6a4fef1
Byron Theobald <byrontheobald@gmail.com>
2017-03-16 16:48:29 +0000

    Added imports for program modules to vtx_core.

--- vtx_core.py
+++ vtx_core.py
@@ -6,6 +6,13 @@ should be split out into appropriate modules and imported here.
 """
 
 # IMPORTS
+import vtx_calc
+import vtx_draw
+import vtx_file
+import vtx_ifce
+
+# TEST MODULES
+# TODO: Remove from final version.
 import vtx_test
 
 vtx_test.module_test()
