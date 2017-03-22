03b485eb2bd2171c5fa1d4ad12cd08f9e28c45da
Oliver Santos-Lopes <ollielopes@live.co.uk>
2017-03-22 14:30:06 +0000

    merged menu bar code into gui file

--- module/vtx_gui.py
+++ module/vtx_gui.py
@@ -21,6 +21,11 @@ uiValues = {
     "count" : None
 }
 
+def donothing():
+    filewin = Toplevel()
+    button = Button(filewin, text='Do nothing')
+    button.pack()
+
 def setDefaults(root):
     uiValues = {
         "rPerm": StringVar(root),
@@ -46,6 +51,36 @@ def initWindow():
 
 def _initMenuBar(_handle):
     """Add menu bar to passed window"""
+    menubar = Menu(_handle)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=donothing)
+    filemenu.add_command(label="Open", command=donothing)
+    filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="Save as...", command=donothing)
+    filemenu.add_command(label="Close", command=donothing)
+
+    filemenu.add_separator()
+
+    filemenu.add_command(label="Exit", command=_handle.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+    editmenu = Menu(menubar, tearoff=0)
+    editmenu.add_command(label="Undo", command=donothing)
+
+    editmenu.add_separator()
+
+    editmenu.add_command(label="Cut", command=donothing)
+    editmenu.add_command(label="Copy", command=donothing)
+    editmenu.add_command(label="Paste", command=donothing)
+    editmenu.add_command(label="Delete", command=donothing)
+    editmenu.add_command(label="Select All", command=donothing)
+
+    menubar.add_cascade(label="Edit", menu=editmenu)
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Help Index", command=donothing)
+    helpmenu.add_command(label="About...", command=donothing)
+    menubar.add_cascade(label="Help", menu=helpmenu)
+
+    _handle.config(menu=menubar)
 
 
 def _initCanvas(_handle):
